d39a878497e22f780eb933999a263f0a2d03fd22
ttytse <34829725+ttytse@users.noreply.github.com>
2018-06-05 10:27:25 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -21,7 +21,9 @@ public class Player {
 		return count;
 	}
 	public boolean contain(Card currentCard)
-	{
+	{    // need to fix
+		for(int i = 0; i < playerhand.size(); i++){
+			if(playerhand.get(i).compareTo(currentCard))
 		return true;
 	}
 	public ArrayList<Card> getList()
